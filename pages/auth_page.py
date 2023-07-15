d98ade75f338fc76029bca164df00c543d3b7d4a
Elena <fox67rus@mail.ru>
2023-07-15 08:42:38 +0300

    класс с локаторами для страницы авторизации

---
+++ pages/auth_page.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.common.by import By
+
+
+class AuthPageLocators:
+    LOCATOR_AUTH_PAGE_TAB_PHONE = (By.ID, 't-btn-tab-phone')
+    LOCATOR_AUTH_PAGE_TAB_EMAIL = (By.ID, 't-btn-tab-mail')
+    LOCATOR_AUTH_PAGE_TAB_LOGIN = (By.ID, 't-btn-tab-login')
+    LOCATOR_AUTH_PAGE_TAB_LS = (By.ID, 't-btn-tab-ls')
+    LOCATOR_AUTH_PAGE_ACTIVE_TAB = (By.CLASS_NAME, 'rt-tab rt-tab--small rt-tab--active')
+
+    LOCATOR_AUTH_PAGE_FIELD_USERNAME = (By.ID, 'username')
+    LOCATOR_AUTH_PAGE_FIELD_PASSWORD = (By.ID, 'password')
+    LOCATOR_AUTH_PAGE_BUTTON_SUBMIT = (By.ID, 'kc-login')
+
+    LOCATOR_AUTH_PAGE_CHECKBOX_REMEMBERME = (By.NAME, 'rememberMe')
+
+    LOCATOR_AUTH_PAGE_BUTTON_FORGOTPASSWORD = (By.ID, 'forgot_password')
+
+
+
+
+
+
+
+
+
+
+
