e85abf514ce20e1fcacbb988d104b2a2716edf68
Elena <fox67rus@mail.ru>
2023-07-26 09:40:34 +0300

    Проверка выбора по умолчанию формы авторизации по телефону

--- tests/test_positive_auth_page.py
+++ tests/test_positive_auth_page.py
@@ -18,14 +18,26 @@ def test_open_auth_page(browser):
     sleep(10)  # для контроля
 
 
+def test_active_tab_is_phone(browser):
+    """
+    Проверка выбора по умолчанию формы авторизации по телефону
+    """
+    auth = AuthPage(browser)
+    auth.go_to_site()
+    assert auth.active_tab_text() == "Телефон"
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_login_with_email(browser):
+    """
+    Проверка авторизации существующего пользователя по электронной почте с помощью кнопки Войти
+    """
     auth = AuthPage(browser)
     auth.go_to_site()
     auth.enter_login(VALID_EMAIL)
     auth.enter_password(VALID_PASSWORD)
     sleep(5)
-    auth.click_to_login_button_()
+    auth.click_to_login_button()
     assert 'account_b2c' in browser.current_url
     browser.save_screenshot('result.png')
-    # assert https://b2c.passport.rt.ru/account_b2c/page?state=e55b0a0e-2f69-4155-ae82-bea8bae6ce4c&client_id=account_b2c#/
     sleep(10)
