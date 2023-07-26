ae2ff98522f4355926fa691696b6f4d08316d0be
Elena <fox67rus@mail.ru>
2023-07-26 09:41:19 +0300

    локатор таба выбора авторизации по умолчанию

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -8,7 +8,7 @@ class AuthPageLocators:
     LOCATOR_AUTH_PAGE_TAB_EMAIL = (By.ID, 't-btn-tab-mail')
     LOCATOR_AUTH_PAGE_TAB_LOGIN = (By.ID, 't-btn-tab-login')
     LOCATOR_AUTH_PAGE_TAB_LS = (By.ID, 't-btn-tab-ls')
-    LOCATOR_AUTH_PAGE_ACTIVE_TAB = (By.CLASS_NAME, 'rt-tab rt-tab--small rt-tab--active')
+    LOCATOR_AUTH_PAGE_ACTIVE_TAB = (By.CLASS_NAME, 'rt-tab--active')
 
     LOCATOR_AUTH_PAGE_FIELD_USERNAME = (By.ID, 'username')
     LOCATOR_AUTH_PAGE_FIELD_PASSWORD = (By.ID, 'password')
@@ -27,5 +27,8 @@ class AuthPage(BasePage):
     def enter_password(self, password_value):
         return self.find_element(AuthPageLocators.LOCATOR_AUTH_PAGE_FIELD_PASSWORD).send_keys(password_value)
 
-    def click_to_login_button_(self):
+    def click_to_login_button(self):
         return self.find_element(AuthPageLocators.LOCATOR_AUTH_PAGE_BUTTON_SUBMIT).click()
+
+    def active_tab_text(self):
+        return self.find_element(AuthPageLocators.LOCATOR_AUTH_PAGE_ACTIVE_TAB).text
