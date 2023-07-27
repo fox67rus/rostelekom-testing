d64841175ce678001dc1c19e582662014ca16194
Elena <fox67rus@mail.ru>
2023-07-27 18:49:12 +0300

    номер службы поддержки в атрибутах класса

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,6 +7,7 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
         self.base_url = 'https://b2c.passport.rt.ru/'
+        self.base_phone = '88001000800'
 
     def go_to_site(self):
         return self.driver.get(self.base_url)
