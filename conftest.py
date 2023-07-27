8641c3a5386d2873eecb9c60d076739cb5613701
Elena <fox67rus@mail.ru>
2023-07-27 17:04:02 +0300

    фикстура очистки буфера обмена

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from ctypes import windll
 
 
 @pytest.fixture(scope="session")
@@ -14,3 +15,11 @@ def browser():
     yield driver
 
     driver.quit()
+
+
+@pytest.fixture(scope="session")
+def clear_clipboard():
+    """Очистка буфера обмена"""
+    if windll.user32.OpenClipboard(None):
+        windll.user32.EmptyClipboard()
+        windll.user32.CloseClipboard()
