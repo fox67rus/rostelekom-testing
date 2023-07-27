ddfe44d6ec10f60536bcd4204555cafa087ec5d1
Elena <fox67rus@mail.ru>
2023-07-27 14:48:06 +0300

    метод - получение текста заголовка формы

--- pages/user_page.py
+++ pages/user_page.py
@@ -4,10 +4,14 @@ from pages.base_page import BasePage
 
 
 class UserPageLocators:
-    LOCATOR_USER_PAGE_TAB_PHONE = (By.ID, 'logout-btn')
+    LOCATOR_USER_PAGE_BUTTON_LOGOUT = (By.ID, 'logout-btn')
+    LOCATOR_USER_PAGE_HEADER = (By.CLASS_NAME, 'card-container__title')
 
 
 class UserPage(BasePage):
 
     def click_logout(self):
-        return self.find_element(UserPageLocators.LOCATOR_USER_PAGE_TAB_PHONE).click()
+        return self.find_element(UserPageLocators.LOCATOR_USER_PAGE_BUTTON_LOGOUT).click()
+
+    def get_header_text(self):
+        return self.find_element(UserPageLocators.LOCATOR_USER_PAGE_HEADER).text
