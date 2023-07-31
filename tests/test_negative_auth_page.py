d7917a332287e68ca1c8a8e53d2ae33dd57c6be6
Elena <fox67rus@mail.ru>
2023-07-31 12:50:10 +0300

    тест - проверка, что при пустом имени пользователя на всех вкладках появляется сообщение об ошибке

--- tests/test_negative_auth_page.py
+++ tests/test_negative_auth_page.py
@@ -7,8 +7,7 @@ from config import *
 from pages.auth_page import AuthPage
 
 
-# 15
-@pytest.mark.current
+# 16
 def test_login_with_invalid_email(browser):
     """
     Проверка авторизации несуществующего пользователя по электронной почте с помощью кнопки Войти
@@ -24,5 +23,35 @@ def test_login_with_invalid_email(browser):
     auth.click_to_checkbox_remember_me()
     auth.click_to_login_button()
 
-    assert auth.get_error_message_text() == 'Неверный логин или пароль'
+    assert auth.get_form_error_message_text() == 'Неверный логин или пароль'
 
+
+# 17
+@pytest.mark.current
+def test_login_with_empty_data(browser):
+    """
+    Проверка авторизации с пустым именем пользователя и паролем
+    """
+    auth = AuthPage(browser)
+
+    # словарь с методами перехода по табу, название таба и сообщение об ошибки при незаполненном имени пользователя
+    tabs_data = {
+        'email': [auth.click_to_tab_email, 'Почта', 'Введите адрес, указанный при регистрации'],
+        'phone': [auth.click_to_tab_phone, 'Телефон', 'Введите номер телефона'],
+        'login': [auth.click_to_tab_login, 'Логин', 'Введите логин, указанный при регистрации'],
+        'ls': [auth.click_to_tab_ls, 'Лицевой счёт', 'Введите номер вашего лицевого счета']
+    }
+
+    auth.go_to_site()
+    sleep(0.5)
+    for tab in tabs_data.keys():
+        tabs_data[tab][0]()
+        assert auth.get_active_tab_text() == tabs_data[tab][1]
+        auth.enter_login('')
+        sleep(0.5)
+        auth.enter_password('')
+        # снять галочку Запомнить меня
+        auth.click_to_checkbox_remember_me()
+        auth.click_to_login_button()
+
+        assert auth.get_error_message_text() == tabs_data[tab][2]
