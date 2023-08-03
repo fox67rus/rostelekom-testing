9fb9405a194c00892ebba332e6eef0cae184d3a7
Elena <fox67rus@mail.ru>
2023-08-03 15:01:58 +0300

    комментарии

--- testdata.py
+++ testdata.py
@@ -12,11 +12,11 @@ def generate_russian_string(length):
 
 
 def chinese_chars():
-    return '的一是不了人我在有他这为之大来以个中上们'
+    return '的一是不了人我在有他这为之大来以个中上们'  # 20 symbols
 
 
 def special_chars():
-    return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'
+    return '|\\/!@#$%^&*()-_=+`~?"№;:[]{}'  # 29 symbols
 
 
 if __name__ == '__main__':
