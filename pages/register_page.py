a30a24d2625dd0e8c20961807b686256eb2957cf
Elena <fox67rus@mail.ru>
2023-08-03 16:59:02 +0300

    очистка полей перед заполнением

--- pages/register_page.py
+++ pages/register_page.py
@@ -25,24 +25,33 @@ class RegisterPage(BasePage):
         self.go_to_site()
 
     def enter_first_name(self, first_name_value):
-        return self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_FIRST_NAME).send_keys(
-            first_name_value)
+        first_name_field = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_FIRST_NAME)
+        first_name_field.send_keys(Keys.CONTROL + "a")
+        first_name_field.send_keys(Keys.DELETE)
+        first_name_field.send_keys(first_name_value)
 
     def enter_last_name(self, last_name_value):
-        return self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_LAST_NAME).send_keys(
-            last_name_value)
+        last_name_field = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_LAST_NAME)
+        last_name_field.send_keys(Keys.CONTROL + "a")
+        last_name_field.send_keys(Keys.DELETE)
+        last_name_field.send_keys(last_name_value)
 
     def enter_user_name(self, user_name_value):
-        return self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_USER_NAME).send_keys(
-            user_name_value)
+        field_user_name = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_USER_NAME)
+        field_user_name.clear()
+        field_user_name.send_keys(user_name_value)
 
     def enter_password(self, password_value):
-        return self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD).send_keys(
-            password_value)
+        field_password = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD)
+        field_password.send_keys(Keys.CONTROL + "a")
+        field_password.send_keys(Keys.DELETE)
+        field_password.send_keys(password_value)
 
     def enter_password_confirm(self, password_confirm_value):
-        return self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD_CONFIRM).send_keys(
-            password_confirm_value)
+        field_password_confirm = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD_CONFIRM)
+        field_password_confirm.send_keys(Keys.CONTROL + "a")
+        field_password_confirm.send_keys(Keys.DELETE)
+        field_password_confirm.send_keys(password_confirm_value)
 
     def click_to_register_button(self):
         return self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_BUTTON_REGISTER).click()
@@ -65,13 +74,3 @@ class RegisterPage(BasePage):
         for element in self.find_elements(RegisterPageLocators.LOCATOR_REGISTER_PAGE_META_ERROR):
             error_message_text.append(element.text)
         return error_message_text
-
-
-
-
-
-
-
-
-
-
