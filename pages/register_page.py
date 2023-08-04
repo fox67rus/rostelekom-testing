c3e20317a7f35940728f416eb1c208a72181e5de
Elena <fox67rus@mail.ru>
2023-08-04 17:38:35 +0300

    метод для очистки формы регистрации, добавление нажатия клавиши TAB в методах ввода данных

--- pages/register_page.py
+++ pages/register_page.py
@@ -29,42 +29,38 @@ class RegisterPage(BasePage):
         first_name_field.send_keys(Keys.CONTROL + "a")
         first_name_field.send_keys(Keys.DELETE)
         first_name_field.send_keys(first_name_value)
+        first_name_field.send_keys(Keys.TAB)
 
     def enter_last_name(self, last_name_value):
         last_name_field = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_LAST_NAME)
         last_name_field.send_keys(Keys.CONTROL + "a")
         last_name_field.send_keys(Keys.DELETE)
         last_name_field.send_keys(last_name_value)
+        last_name_field.send_keys(Keys.TAB)
 
     def enter_user_name(self, user_name_value):
         field_user_name = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_USER_NAME)
-        field_user_name.clear()
+        field_user_name.send_keys(Keys.CONTROL + "a")
+        field_user_name.send_keys(Keys.DELETE)
         field_user_name.send_keys(user_name_value)
+        field_user_name.send_keys(Keys.TAB)
 
     def enter_password(self, password_value):
         field_password = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD)
         field_password.send_keys(Keys.CONTROL + "a")
         field_password.send_keys(Keys.DELETE)
         field_password.send_keys(password_value)
+        field_password.send_keys(Keys.TAB)
 
     def enter_password_confirm(self, password_confirm_value):
         field_password_confirm = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD_CONFIRM)
         field_password_confirm.send_keys(Keys.CONTROL + "a")
         field_password_confirm.send_keys(Keys.DELETE)
         field_password_confirm.send_keys(password_confirm_value)
+        field_password_confirm.send_keys(Keys.TAB)
 
     def click_to_register_button(self):
         return self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_BUTTON_REGISTER).click()
-
-    def clear_first_name_field(self):
-        first_name_field = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_FIRST_NAME)
-        first_name_field.send_keys(Keys.CONTROL + "a")
-        first_name_field.send_keys(Keys.DELETE)
-
-    def clear_last_name_field(self):
-        last_name_field = self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_LAST_NAME)
-        last_name_field.send_keys(Keys.CONTROL + "a")
-        last_name_field.send_keys(Keys.DELETE)
 
     def get_header_h1_text(self):
         return self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_H1).text
@@ -75,9 +71,16 @@ class RegisterPage(BasePage):
             error_message_text.append(element.text)
         return error_message_text
 
-    def reset_meta_errors(self):
-        self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_FIRST_NAME).click()
-        self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_LAST_NAME).click()
-        self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_USER_NAME).click()
-        self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD).click()
-        self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD_CONFIRM).click()
+    def clear_registration_form(self):
+        registration_form = [
+            self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_FIRST_NAME),
+            self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_LAST_NAME),
+            self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_USER_NAME),
+            self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD),
+            self.find_element(RegisterPageLocators.LOCATOR_REGISTER_PAGE_FIELD_PASSWORD_CONFIRM)
+        ]
+
+        for field in registration_form:
+            field.send_keys(Keys.CONTROL + "a")
+            field.send_keys(Keys.DELETE)
+
