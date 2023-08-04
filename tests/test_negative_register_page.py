4a189cbfe4a1244ac9b3ec2f3f876e5bf60016df
Elena <fox67rus@mail.ru>
2023-08-04 17:39:28 +0300

    переименование метода; тест поля E-mail или телефон

--- tests/test_negative_register_page.py
+++ tests/test_negative_register_page.py
@@ -6,7 +6,6 @@ from pages.register_page import RegisterPage
 from testdata import special_chars
 
 
-@pytest.mark.current
 @pytest.mark.register
 def test_register_with_empty_data(browser, go_to_register_page):
     """
@@ -30,7 +29,7 @@ def test_register_with_empty_data(browser, go_to_register_page):
     assert register.get_header_h1_text() == 'Регистрация'
     assert len(
         register.get_meta_error_message()) == 5, 'Количество сообщений об ошибках не соответствует количеству полей'
-    register.reset_meta_errors()  # сброс ошибок для полей
+    register.clear_registration_form()  # очистка полей формы
     # sleep(5)  # для контроля
 
 
@@ -49,10 +48,34 @@ def test_field_first_name(browser, first_name_value, go_to_register_page):
     register = RegisterPage(browser)
     sleep(0.5)  # антикапча
     register.enter_first_name(first_name_value)
-    sleep(0.5)  # антикапча
-    register.enter_last_name("Фамилия")
+    # sleep(0.5)  # антикапча
+    # register.enter_last_name("Фамилия")
 
     assert register.get_meta_error_message()[0] == 'Необходимо заполнить поле кириллицей. От 2 до 30 символов.'
+    sleep(3)  # для контроля
+
+    assert register.get_header_h1_text() == 'Регистрация'
+    register.clear_registration_form()  # очистка полей формы
+
+
+@pytest.mark.current
+@pytest.mark.register
+@pytest.mark.parametrize(
+    "user_name_value",
+    ["А", "Оченьоченьдлинноеимядлятеставот", "Michael", "袁世凱", "12345", special_chars()],
+    ids=["1 symbol", "31 symbol", "in English", "china", "digit", "special_chars"]
+)
+def test_field_user_name(browser, user_name_value, go_to_register_page):
+    """
+    Проверка, что при вводе недопустимых значений в поле E-mail или мобильный телефон возникает сообщение об ошибке
+    """
+
+    register = RegisterPage(browser)
+    sleep(0.5)  # антикапча
+    register.enter_user_name(user_name_value)
+
+    assert register.get_meta_error_message()[
+               0] == 'Введите телефон в формате +7ХХХХХХХХХХ или +375XXXXXXXXX, или email в формате example@email.ru'
     sleep(3)  # для контроля
 
     assert register.get_header_h1_text() == 'Регистрация'
