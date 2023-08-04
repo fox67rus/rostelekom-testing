40d5bc21a1fad0927aeff374d16a1b5ffacb3a38
Elena <fox67rus@mail.ru>
2023-08-04 19:13:25 +0300

    Проверка отсутствия ошибок при корректном заполнении поля Имя

--- tests/test_positive_register_page.py
+++ tests/test_positive_register_page.py
@@ -3,12 +3,13 @@ from time import sleep
 import pytest
 
 from pages.register_page import RegisterPage
+from testdata import generate_russian_string
 
 
 @pytest.mark.register
 def test_register_with_correct_data(browser, go_to_register_page, faker):
     """
-    Проверка регистрации с корректными данными
+    Проверка, что при нажатии на кнопку Зарегистрироваться с заполненными корректными данными происходит отправка кода подтверждения
     """
     register = RegisterPage(browser)
     sleep(0.5)
@@ -28,4 +29,28 @@ def test_register_with_correct_data(browser, go_to_register_page, faker):
     sleep(0.5)
 
     assert register.get_header_h1_text() == 'Подтверждение email'
-    sleep(5)  # для контроля
+    # sleep(3)  # для контроля
+
+
+@pytest.mark.current
+@pytest.mark.register
+@pytest.mark.parametrize(
+    "first_name_value",
+    ["Иван", "Ян", "Лия", generate_russian_string(29), generate_russian_string(30)],
+    ids=["Common", "2 symbols", "3 symbols", "29 symbols", "30 symbols"]
+)
+def test_field_first_name(browser, first_name_value, go_to_register_page):
+    """
+    Проверка, что при вводе допустимых значений в поле Имя не возникает сообщения об ошибке
+    """
+
+    register = RegisterPage(browser)
+    sleep(0.5)  # антикапча
+    register.enter_first_name(first_name_value)
+    sleep(0.5)  # антикапча
+    register.click_to_register_button()
+
+    sleep(3)  # для контроля
+    assert len(
+        register.get_meta_error_message()) == 4, 'Появилось сообщение об ошибке'
+    register.clear_registration_form()  # очистка полей формы
